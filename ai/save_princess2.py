c069465e9ad27f31d69f108ce55b0d118ac413c1
cheshirecat12 <anthony.gillioz@outlook.com>
2019-09-01 11:01:55 +0200

    add the second version of the save princess 2 exercise

--- ai/save_princess2.py
+++ ai/save_princess2.py
@@ -12,10 +12,48 @@ Hackerrank exercise: save princess 2. In this exercise the bot must find
 https://www.hackerrank.com/challenges/saveprincess2/problem
 """
 
+# VERSION 2
+from operator import lt, gt
+
+
+class Point():
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+def _choose_move(pos_b, pos_p, op, attr):
+    return op(getattr(pos_p, attr),
+              getattr(pos_b, attr))
+
+
+MOVES = [("DOWN", gt, "y"),
+         ("UP", lt, "y"),
+         ("RIGHT", gt, "x"),
+         ("LEFT", lt, "x")]
 PRINCESS = "p"
 
 
 def nextMove(n, bot_pos_x, bot_pos_y, grid):
+    bot_pos = Point(bot_pos_x, bot_pos_y)
+    princess_pos = [(row.find(PRINCESS), i)
+                    for i, row in enumerate(grid)
+                    if PRINCESS in row][0]
+    princess_pos = Point(*princess_pos)
+
+    for move, op, attr in MOVES:
+        if _choose_move(bot_pos, princess_pos, op, attr):
+            return move
+
+    return "Here I am!"
+
+
+# Version 1
+PRINCESS = "p"
+
+
+def nextMove1(n, bot_pos_x, bot_pos_y, grid):
     princess_pos_x, princess_pos_y = [(row.find(PRINCESS), i)
                                       for i, row in enumerate(grid)
                                       if PRINCESS in row][0]
