3f105f4b42b1d0b197b260e734be1af9b490edd8
cheshirecat12 <anthony.gillioz@outlook.com>
2020-03-28 16:44:48 +0100

    probelm 10

--- eulerproject/problem010.py
+++ eulerproject/problem010.py
@@ -1,28 +1,42 @@
 #!/bin/python3
-from itertools import takewhile
 
 
-def get_primes(num):
-    """Compute the primes up to num."""
-    primes = [2]
-    candidate = 3
-    while candidate <= num:
-        if all(candidate % prime for prime in primes):
-            primes.append(candidate)
-        candidate += 2
+def sieve_of_eratosthenes(num):
+    primes = [True] * (num+1)
+
+    candidate = 2
+    while candidate**2 <= num:
+        if primes[candidate]:
+            primes[candidate**2::candidate] = [False] *\
+                    len(primes[candidate**2::candidate])
+
+        candidate += 1
+    primes[:2] = [False, False]
 
     return primes
 
 
+def sum_of_primes(primes):
+    tmp = []
+    count = 0
+    for i, is_prime in enumerate(primes):
+        if is_prime:
+            count += i
+        tmp.append(count)
+
+    return tmp
+
+
 def main():
     t = int(input().strip())
-    num_2_check = [int(input().strip()) for _ in range(t)]
-    largest_num = max(num_2_check)
-    primes = get_primes(largest_num)
+    largest_num = 10**6
 
-    for num in num_2_check:
-        sum_of_primes = sum(takewhile(lambda x: x <= num, primes))
-        print(sum_of_primes)
+    primes = sieve_of_eratosthenes(largest_num)
+    sum_ = sum_of_primes(primes)
+
+    for _ in range(t):
+        n = int(input().strip())
+        print(sum_[n])
 
 
 if __name__ == "__main__":
