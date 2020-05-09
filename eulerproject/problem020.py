880467b0e7a8e06acf8f365a7206eed666661b70
cheshirecat12 <anthony.gillioz@outlook.com>
2020-05-09 17:56:35 +0200

    euler problem 20 brute force

---
+++ eulerproject/problem020.py
@@ -0,0 +1,20 @@
+def create_factorial(num):
+    factorials = [1]
+    for i in range(1, num+1):
+        factorials.append(factorials[-1]*i)
+
+    return factorials
+
+
+def main():
+    nb_tests = int(input().strip())
+    num_2_test = [int(input()) for _ in range(nb_tests)]
+    num_max = max(num_2_test)
+    factorials = create_factorial(num_max)
+
+    for num in num_2_test:
+        print(sum(int(val) for val in str(factorials[num])))
+
+
+if __name__ == '__main__':
+    main()
